bdfce590e1fa96e241cc2bece67add320dd476f5
albertjacobsz <dekater403@gmail.com>
2024-02-03 12:57:09 +0800

    preparation for walking

--- main.py
+++ main.py
@@ -5,98 +5,136 @@ from adafruit_motor import servo
 from adafruit_servokit import ServoKit
 import time
 
+from spotmicroai.utilities.general import General
+
 i2c = busio.I2C(SCL, SDA)
 
-pca_2 = PCA9685(i2c, address=0x40,reference_clock_speed=25000000)
-pca_2.frequency = 50
-#shoulder
-#lr
-lrShoulder = servo.Servo(pca_2.channels[6])
 
-lrShoulder.set_pulse_width_range(500,2500)
-#rr
-rrShoulder = servo.Servo(pca_2.channels[9])
-rrShoulder.set_pulse_width_range(500,2500)
-#lf
+class SpotMicro:
+    def __init__(self):
+        self.pca_2 = PCA9685(i2c, address=0x40, reference_clock_speed=25000000)
+        self.pca_2.frequency = 50
+        # shoulder
+        # lr
+        self.lrShoulder = servo.Servo(self.pca_2.channels[6])
 
-#leg
-#lr
-lrLeg = servo.Servo(pca_2.channels[7])
-lrLeg.set_pulse_width_range(500,2500)
-#rr
-rrLeg = servo.Servo(pca_2.channels[10])
-rrLeg.set_pulse_width_range(500,2500)
-#feet
-#lr
-lrFeet = servo.Servo(pca_2.channels[8])
-lrFeet.set_pulse_width_range(500,2500)
-#rr
-rrFeet = servo.Servo(pca_2.channels[11])
-rrFeet.set_pulse_width_range(500,2500)
+        self.lrShoulder.set_pulse_width_range(500, 2500)
+        # rr
+        self.rrShoulder = servo.Servo(self.pca_2.channels[9])
+        self.rrShoulder.set_pulse_width_range(500, 2500)
+        # lf
+
+        # leg
+        # lr
+        self.lrLeg = servo.Servo(self.pca_2.channels[7])
+        self.lrLeg.set_pulse_width_range(500, 2500)
+        # rr
+        self.rrLeg = servo.Servo(self.pca_2.channels[10])
+        self.rrLeg.set_pulse_width_range(500, 2500)
+        # feet
+        # lr
+        self.lrFeet = servo.Servo(self.pca_2.channels[8])
+        self.lrFeet.set_pulse_width_range(500, 2500)
+        # rr
+        self.rrFeet = servo.Servo(self.pca_2.channels[11])
+        self.rrFeet.set_pulse_width_range(500, 2500)
+
+        self.lfShoulder = servo.Servo(self.pca_2.channels[0])
+        self.lfShoulder.set_pulse_width_range(500, 2500)
+        # rf
+        self.rfShoulder = servo.Servo(self.pca_2.channels[3])
+        self.rfShoulder.set_pulse_width_range(500, 2500)
+        # lf
+        self.lfLeg = servo.Servo(self.pca_2.channels[1])
+        self.lfLeg.set_pulse_width_range(500, 2500)
+        # rf
+        self.rfLeg = servo.Servo(self.pca_2.channels[4])
+        self.rfLeg.set_pulse_width_range(500, 2500)
+        # lf
+        self.lfFeet = servo.Servo(self.pca_2.channels[2])
+        self.lfFeet.set_pulse_width_range(500, 2500)
+        # rf
+        self.rfFeet = servo.Servo(self.pca_2.channels[5])
+        self.rfFeet.set_pulse_width_range(500, 2500)
 
 
+spot = SpotMicro()
 
-lfShoulder = servo.Servo(pca_2.channels[0])
-lfShoulder.set_pulse_width_range(500,2500)
-#rf
-rfShoulder= servo.Servo(pca_2.channels[3])
-rfShoulder.set_pulse_width_range(500,2500)
-#lf
-lfLeg = servo.Servo(pca_2.channels[1])
-lfLeg.set_pulse_width_range(500,2500)
-#rf
-rfLeg= servo.Servo(pca_2.channels[4])
-rfLeg.set_pulse_width_range(500,2500)
-#lf
-lfFeet = servo.Servo(pca_2.channels[2])
-lfFeet.set_pulse_width_range(500,2500)
-#rf
-rfFeet= servo.Servo(pca_2.channels[5])
-rfFeet.set_pulse_width_range(500,2500)
 
 def standing():
     variation_leg = 50
     variation_feet = 70
     time.sleep(1)
 
-    lrShoulder.angle = 85
-    lrLeg.angle = 90 - variation_leg - 10
-    lrFeet.angle = 0 + variation_feet
+    spot.lrShoulder.angle = 85
+    spot.lrLeg.angle = 90 - variation_leg - 10
+    spot.lrFeet.angle = 0 + variation_feet
 
-    rrShoulder.angle = 95
-    rrLeg.angle = 90 + variation_leg + 10
-    rrFeet.angle = 180 - variation_feet
+    spot.rrShoulder.angle = 95
+    spot.rrLeg.angle = 90 + variation_leg + 10
+    spot.rrFeet.angle = 180 - variation_feet
 
     time.sleep(0.05)
 
-    lfShoulder.angle = 90
-    lfLeg.angle = 90 - variation_leg  -10
-    lfFeet.angle = 0 + variation_feet  
+    spot.lfShoulder.angle = 90
+    spot.lfLeg.angle = 90 - variation_leg - 10
+    spot.lfFeet.angle = 0 + variation_feet
 
-    rfShoulder.angle = 90
-    rfLeg.angle = 90 + variation_leg + 10
-    rfFeet.angle = 180 - variation_feet + 5
+    spot.rfShoulder.angle = 90
+    spot.rfLeg.angle = 90 + variation_leg + 10
+    spot.rfFeet.angle = 180 - variation_feet + 5
+
 
 def rest():
 
-    lrShoulder.angle = 90 
-    lrLeg.angle = 90
-    lrFeet.angle = 0
+    spot.lrShoulder.angle = 90
+    spot.lrLeg.angle = 90
+    spot.lrFeet.angle = 0
 
-    rrShoulder.angle = 90
-    rrLeg.angle = 90
-    rrFeet.angle = 180
+    spot.rrShoulder.angle = 90
+    spot.rrLeg.angle = 90
+    spot.rrFeet.angle = 180
 
     time.sleep(0.05)
 
-    lfShoulder.angle = 90
-    lfLeg.angle = 90
-    lfFeet.angle = 0 
+    spot.lfShoulder.angle = 90
+    spot.lfLeg.angle = 90
+    spot.lfFeet.angle = 0
 
-    rfShoulder.angle = 90 
-    rfLeg.angle = 90
-    rfFeet.angle = 180
+    spot.rfShoulder.angle = 90
+    spot.rfLeg.angle = 90
+    spot.rfFeet.angle = 180
 
 
+def body_move_body_left_right(raw_value):
+    range = 5
 
+    if raw_value < 0:
+        spot.lrShoulder -= range
+        spot.rrShoulder -= range
+        spot.lfShoulder += range
+        spot.rfShoulder += range
+
+    elif raw_value > 0:
+        spot.lrShoulder += range
+        spot.rrShoulder += range
+        spot.lfShoulder -= range
+        spot.rfShoulder -= range
+
+    else:
+        rest()
+
+
+def body_move_body_left_right_analog(raw_value):
+
+    delta_a = int(General().maprange((-1, 1), (30, 150), raw_value))
+    delta_b = int(General().maprange((-1, 1), (150, 30), raw_value))
+
+    spot.lrShoulder = delta_a
+    spot.rrShoulder = delta_a
+    spot.lfShoulder = delta_b
+    spot.rfShoulder = delta_b
+
+
+body_move_body_left_right_analog(10)
 rest()
