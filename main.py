306d74b725a46e633aaf63e202696bea58d24809
albertjacobsz <dekater403@gmail.com>
2024-02-07 13:26:13 +0800

    production mode

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from spot import Spot
+from spot.spot import Spot
 import argparse
 
 
@@ -13,17 +13,18 @@ args = parser.parse_args()
 def debug_mode():
     while True:
         uInput = int(input("enter a number between 0-11, enter -1 to exit"))
-        if uInput > 11 or uInput < 0:
-            print("invalid number choice")
-        elif uInput <= 11 and uInput >= 0:
+
+        if uInput <= 11 and uInput >= 0:
             print("ready for calibration")
             angleInput = int(input("enter in an angle"))
             if angleInput >= 0 and angleInput <= 180:
-                spot.calibrate_angle(spot.servoDictionary[uInput])
+                spot.calibrate_angle(spot.servoDictionary[uInput], angleInput)
             else:
                 print("the angle chosen is not correct")
         elif uInput == -1:
             return
+        elif uInput > 11 or uInput < 0:
+            print("invalid number choice")
 
 
 if __name__ == "__main__":
@@ -31,6 +32,12 @@ if __name__ == "__main__":
     if args.mode == 1:
         debug_mode()
     elif args.mode == 0:
-        print("production")
+        while True:
+            uInput = int(
+                input("Press 1 for rest, 2 for standing, 3 for walking, -1"))
+            if uInput == 1:
+                spot.rest()
+            elif uInput == 2:
+                spot.standing()
     else:
         print("Theres been an error")
